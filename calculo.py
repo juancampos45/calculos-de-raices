5d634bd994dcc609304a340b5f53b65eafd7d70f
juancampos45 <55142610+juancampos45@users.noreply.github.com>
2019-09-12 10:16:09 -0400

    Add files via upload

---
+++ calculo.py
@@ -0,0 +1,23 @@
+# Proposito: Trabajar con los operadores aritmrtico 
+
+# Entrada de datos
+a = float(input('ingrese el primer valo: '))
+
+b = float(input('ingrese el segundo valor: '))
+
+c = float(input('ingrese tercer valor: '))
+
+d = float(input('ingrese el cuarto valor: '))
+
+e = float(input('ingrese el quinto valor: '))
+
+#Prosedimiento
+p = ((-b +((a-b)**2 -(4*a*c))**(1/2)))/(2*a)
+
+#Salida de datos
+print('las raices son= ', p)
+
+
+#Fin del programa
+input('Pulse una tecla para finalizar...') # pausa
+
